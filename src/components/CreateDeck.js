f756cb5a93bdd96a6634a3c34ec307f5d8b8cd31
Samuel Vinnik <themansamuel3@gmail.com>
2023-09-08 18:20:59 -0500

    form added

--- src/components/CreateDeck.js
+++ src/components/CreateDeck.js
@@ -1,12 +1,22 @@
+import { Link } from "react-router-dom";
+import "./CreateDeck.css";
 
-
-
-function CreateDeck(){
-
-    return(
-        <p>hello</p>
-    )
-
+function CreateDeck() {
+  return (
+    <div>
+      <h1>Create Deck</h1>
+      <form>
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Description</label>
+        <textarea type="text" name="description" />
+      </form>
+      <Link to="/">
+        <button>Cancel</button>
+      </Link>
+      <button>Submit</button>
+    </div>
+  );
 }
 
 export default CreateDeck;
