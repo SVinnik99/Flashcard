73e9f826e7c35281b8f21c04b8241429fe52f61f
Carlo Santos <rcarlosantos89@gmail.com>
2023-09-12 13:10:04 -0500

    listDecks and decks state()

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Change Log
+
+9.12.23 1:10pm
+- Added readme
+- updated index.js
+- working listDecks utility function
+- create state for decks
+- using async await style to grab the list of decks.
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { listDecks } from "../utils/api";
+import { useState } from "react";
 
 function Layout() {
+
+  let [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    async function fetchDecks() {
+      try {
+        const response = await listDecks();
+        setDecks(response);
+      } catch (error) {
+        console.error("Error fetching decks: ", error)
+      }
+    }
+
+    fetchDecks();
+  }, [])
+
   return (
     <>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
+        <p>{decks[0].name}</p>
         <NotFound />
       </div>
     </>
